ffa631b93384ada7ee3d67609a24046659bcaaad
Sarah Mei <sarahmei@gmail.com>
2009-08-30 14:32:55 -0700

    Adding calculation of shopping list items

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -4,6 +4,7 @@ class ShoppingList < ActiveRecord::Base
   accepts_nested_attributes_for :recipe
 
   def list_items
-    []
+    recipe.ingredients.collect(&:food_item) -
+      (user.inventory.pantry_items.collect(&:food_item) || [])
   end
 end
